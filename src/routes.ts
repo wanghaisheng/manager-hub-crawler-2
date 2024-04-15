ad6326546f5240567cc390cc5ad63ec5d2e3a7f7
zeallat <zeallat94@gmail.com>
2024-04-16 03:41:36 +0900

    feat(routes): 로그인 성공 시 스냅샷 추가

--- src/routes.ts
+++ src/routes.ts
@@ -79,5 +79,6 @@ router.addHandler('login', async ({ request, page, log, pushData }) => {
     UtilService.log('Submit search form')
     await page.waitForURL('**\/main*')
     UtilService.log('login success')
+    UtilService.snapshot(page, 'login-success')
 
 });
