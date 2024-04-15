146ba70da69c61598f33af61f6f020917a2f2d5b
zeallat <zeallat94@gmail.com>
2024-04-16 00:29:46 +0900

    feat: playwright-extra 및 stealth 플러그인 추가하여 크롤링 은닉성 강화

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,19 @@ import { PlaywrightCrawler, ProxyConfiguration } from 'crawlee';
 
 import { router } from './routes.js';
 
+// playwright-extra is a drop-in replacement for playwright,
+// it augments the installed playwright with plugin functionality
+import {chromium} from "playwright-extra";
+
+import puppeteerExtraPluginStealth from 'puppeteer-extra-plugin-stealth'
+
+// Load the stealth plugin and use defaults (all tricks to hide playwright usage)
+// Note: playwright-extra is compatible with most puppeteer-extra plugins
+const stealth = puppeteerExtraPluginStealth()
+
+// Add the plugin to playwright (any number of plugins can be added)
+chromium.use(stealth)
+
 const startUrls = ['https://crawlee.dev'];
 
 const crawler = new PlaywrightCrawler({
@@ -10,6 +23,12 @@ const crawler = new PlaywrightCrawler({
     requestHandler: router,
     // Comment this option to scrape the full website.
     maxRequestsPerCrawl: 20,
+    launchContext: {
+        launcher: chromium,
+        launchOptions: {
+            headless: true
+        }
+    }
 });
 
 await crawler.run(startUrls);
