4ef05d68f44846ded646c9b2c048ef5df9fa7ae3
zeallat <zeallat94@gmail.com>
2024-04-16 01:18:41 +0900

    feat: 플레이라이트 익명화 및 리캡챠 플러그인 추가

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,26 @@ import { router } from './routes.js';
 import {chromium} from "playwright-extra";
 
 import puppeteerExtraPluginStealth from 'puppeteer-extra-plugin-stealth'
+import puppeteerExtraPluginRecaptcha from 'puppeteer-extra-plugin-recaptcha'
+import puppeteerExtraPluginAnonymizeUa from 'puppeteer-extra-plugin-anonymize-ua'
+
 
 // Load the stealth plugin and use defaults (all tricks to hide playwright usage)
 // Note: playwright-extra is compatible with most puppeteer-extra plugins
-const stealth = puppeteerExtraPluginStealth()
-
 // Add the plugin to playwright (any number of plugins can be added)
-chromium.use(stealth)
+chromium.use(puppeteerExtraPluginStealth())
+
+// @ts-ignore
+chromium.use(puppeteerExtraPluginAnonymizeUa())
+
+chromium.use(
+// @ts-ignore
+    puppeteerExtraPluginRecaptcha({
+        provider: { id: '2captcha', token: '03be056750ef2d44999aac82101409c9' },
+        visualFeedback: true
+    })
+)
+
 
 const startUrls = ['https://crawlee.dev'];
 
